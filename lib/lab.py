516bdc3ad454f1c1852ce40b407f145c673bf07b
“jennifernjeri” <jenniferliz030@gmail.com>
2023-05-22 20:06:51 +0300

    PYTHON PRACTICE

---
+++ lib/lab.py
@@ -0,0 +1,10 @@
+full_name = "Jennifer Njeri"
+print(full_name)
+email = "jennifer.njeri@student.moringaschool.com"
+print(email)
+website = "www.moringa.com"
+print (website)
+school_address = "Victoria Courts"
+print(type(school_address))
+"Send an email to " + full_name + " at " + email + " to say how nice it was meeting yesterday."
+"Go to " + website + " and download " + school_address + " to " + email
