e209a002a4df588bbbce121d8b6e16cf3eb86cff
“jennifernjeri” <jenniferliz030@gmail.com>
2023-05-24 19:30:57 +0300

    RECAP FILE BY TM - MARYANNE

--- lib/lab.py
+++ lib/lab.py
@@ -8,3 +8,13 @@ school_address = "Victoria Courts"
 print(type(school_address))
 "Send an email to " + full_name + " at " + email + " to say how nice it was meeting yesterday."
 "Go to " + website + " and download " + school_address + " to " + email
+travel_month = "January"
+travel_month
+number_of_weeks = "5"
+number_of_weeks
+traveling_schedule = "I will be travelling " + number_of_weeks + " weeks starting in the month of " + travel_month
+traveling_schedule
+vacation_days = 0
+vacation_days += 1
+vacation_days += 1
+vacation_days
